0997f7ed85f45d65997a928541d65067afb7f0aa
passapol <phasphlsuthahthrrm976@gmail.com>
2025-07-08 08:35:02 +0700

    db

--- dbconnect.ts
+++ dbconnect.ts
@@ -2,8 +2,8 @@ import mysql from "mysql";
 
 export const conn = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "passapol",
-  password: "123456",
-  database: "app_db",
+  host: "202.28.34.197",
+  user: "tripbooking",
+  password: "tripbooking",
+  database: "tripbooking",
 });
